7f04234cce0f7cce7725c849d9e1ddcc06aaaa02
Thomas Diemel <thomas.diemel@hva.nl>
2024-10-27 13:44:38 +0100

    turned off debug mode

--- lib/game.dart
+++ lib/game.dart
@@ -12,7 +12,7 @@ class MazeRunnerGame extends FlameGame with HasKeyboardHandlerComponents {
   @override
   Future<void> onLoad() async {
     await super.onLoad();
-    debugMode = true;
+    debugMode = false;
 
     // Add maze
     maze = Maze(32, 50, 27, this);
